447e1a76fec4a611b04c365b7f627db2a19ea3b7
andon\13521 <fhuan123@126.com>
2018-01-30 17:24:00 +0800

    slice test123

--- MysqlDemo/src/Test/Test.go
+++ MysqlDemo/src/Test/Test.go
@@ -2,31 +2,33 @@ package main
 
 import (
 	"AppStart"
-	//"fmt"
-	//"strconv"
+	"fmt"
+	"strconv"
+	"fmt"
+	"strconv"
 )
 
 func main() {
 	AppStart.AppInit()
-	//slice1 := make([]string,3,5)
-	//
-	//slice1[0] = "0"
-	//slice1[1] = "1"
-	//slice1[2] = "2"
-	//
-	//
-	//fmt.Println(strconv.Itoa(len(slice1)) + "----------"  + strconv.Itoa(cap(slice1)))
-	//
-	//slice2 := slice1
-	//
-	//slice1 = append(slice1,"3")
-	//slice1 = append(slice1,"4")
-	//
-	//fmt.Println(strconv.Itoa(len(slice1)) + "----------"  + strconv.Itoa(cap(slice1)))
-	//slice1 = append(slice1,"5")
-	//slice1 = append(slice1,"6")
-	//fmt.Println(strconv.Itoa(len(slice1)) + "----------"  + strconv.Itoa(cap(slice1)))
-	//fmt.Println(strconv.Itoa(len(slice2)) + "----------"  + strconv.Itoa(cap(slice2)))
+	slice1 := make([]string,3,5)
+
+	slice1[0] = "0"
+	slice1[1] = "1"
+	slice1[2] = "2"
+
+
+	fmt.Println(strconv.Itoa(len(slice1)) + "----------"  + strconv.Itoa(cap(slice1)))
+
+	slice2 := slice1
+
+	slice1 = append(slice1,"3")
+	slice1 = append(slice1,"4")
+
+	fmt.Println(strconv.Itoa(len(slice1)) + "----------"  + strconv.Itoa(cap(slice1)))
+	slice1 = append(slice1,"5")
+	slice1 = append(slice1,"6")
+	fmt.Println(strconv.Itoa(len(slice1)) + "----------"  + strconv.Itoa(cap(slice1)))
+	fmt.Println(strconv.Itoa(len(slice2)) + "----------"  + strconv.Itoa(cap(slice2)))
 
 
 }
